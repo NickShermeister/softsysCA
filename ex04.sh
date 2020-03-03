dc005f5f376e380983e337755da19b00c08612c3
NickShermeister <ncsherman159@gmail.com>
2020-03-02 20:27:39 -0500

    almost done with ex04 script.

--- ex04.sh
+++ ex04.sh
@@ -6,35 +6,54 @@ curr=$PWD
 # trout: all .c files exist: 2 pts; compiles: 2 points; compiles w/out warnings: 3 points
 # Unit testing: test_util.c exists: 1 pt; tests a function: 2 pts
 
+outfile="${curr}/notes.txt"
+
+echo "Beginning Grading ex04" >> $outfile
+
 for (( i=0; i<$len; i++ ));
 do
 if cd "${allNames[$i]}/ExercisesInC/exercises/ex04"; then
+  echo "Beginning Grading ${allNames[$i]}" >> $outfile
   echo $(pwd)
   if cd "trout/"; then
     echo "trout"
     rm *.o
     less Makefile
     ls
+    read  -p "${allNames[$i]}\n Makefile Notes: " output
+    echo "${allNames[$i]}\n Makefile Notes: " >> $outfile
+    echo $output >> $outfile
     read  -p "Pick command\n" output
     make "${output}"
     if less "test_util.c"; then
-      gcc test_util.c
+      gcc test_util.c -o testout
       chmod 777 testout
       ./testout
+      read  -p "${allNames[$i]}\n test_util.c: " output
+      echo $output >> $outfile
+
     else
       if less "../test_util.c"; then
         gcc ../test_util.c -o testout
         chmod 777 ../testout
         ./../testout
+        read  -p "${allNames[$i]}\n test_util.c: " output
+        echo $output >> $outfile
       else
         echo "test_util.c DNE at this level"
+        read  -p "${allNames[$i]}\n test_util.c: " output
+        echo $output >> $outfile
       fi
     fi
     cd ..
   else
     echo "No trout found."
+    read  -p "${allNames[$i]}\n trout DNE: " output
+    echo $output >> $outfile
   fi
 
+  read  -p "${allNames[$i]}\n overall notes: " output
+  echo $output >> $outfile
   cd "${curr}"
 else
   echo "Something wrong with ${allNames[$i]}"
